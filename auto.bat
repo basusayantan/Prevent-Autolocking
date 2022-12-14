a96b85bfd10740af346da146b5258668b3770084
Sayantan Basu <103572971+basusayantan@users.noreply.github.com>
2022-12-15 05:12:33 +0530

    Update auto.bat

--- auto.bat
+++ auto.bat
@@ -1,3 +1,3 @@
 @echo off
-powershell.exe -executionpolicy remotesigned -command "C:\Users\a882057\Downloads\script.ps1 -minutes 180 -keyboard"
+powershell.exe -executionpolicy remotesigned -command "C:\Users\<Local User>\Downloads\script.ps1 -minutes 180 -keyboard"
 pause
